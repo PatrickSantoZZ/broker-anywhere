2da770be496a1ce61a14620278b108e4e3069691
Nekonekoo <patrickfetscher60@gmail.com>
2019-05-10 23:09:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,32 +3,44 @@
 	
 	
 	mod.command.add('broker', () => {
-        mod.send('S_NPC_MENU_SELECT', 1 , {type:28})
-    
+	mod.send('S_NPC_MENU_SELECT', 1 , {type:28})
+		
+	});
+
 	
-	mod.command.add('fishchest', () => {
-        mod.send('S_NPC_MENU_SELECT', 1 , {type:1001})
+			/*	mod.command.add('fishchest', () => { 
+		mod.send('S_NPC_MENU_SELECT', 1 , {type:1001})
 	
-	mod.command.add('fishboy', () => {
+		
+		
+		mod.command.add('fishboy', () => {
         mod.send('S_NPC_MENU_SELECT', 1 , {type:1000})
-		
-	mod.command.add('signumtrader', () => {
-        mod.send('S_NPC_MENU_SELECT', 1 , {type:16095})
-		
-	mod.command.add('trader', () => {
-        mod.send('S_NPC_MENU_SELECT', 1 , {type:16062})
+		};	
 	
-	mod.command.add('fishtrader', () => {
+		mod.command.add('signumtrader', () => {
+        mod.send('S_NPC_MENU_SELECT', 1 , {type:16095})
+	
+		};	
+	
+		mod.command.add('trader', () => {
+        mod.send('S_NPC_MENU_SELECT', 1 , {type:16062})
+
+		};
+	
+		mod.command.add('fishtrader', () => {
         mod.send('S_NPC_MENU_SELECT', 1 , {type:16094})
 	
-	mod.command.add('alchi', () => {
-        mod.send('S_NPC_MENU_SELECT', 1 , {type:193})
+		};
 	
-	mod.command.add('warehouse', () => {
+		mod.command.add('alchi', () => {
+		mod.send('S_NPC_MENU_SELECT', 1 , {type:193})
+		};
+	
+		mod.command.add('warehouse', () => {
         mod.send('S_NPC_MENU_SELECT', 1 , {type:4})
 	
+		});  */
 	
-	});
 };
 
 
